9b3626f5d3b49420a860d8d9bb4c3c4c69a8d242
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-12-07 13:21:20 +0200

    Refactor tests a bit - use model-baker

--- expenses/tests/test_views.py
+++ expenses/tests/test_views.py
@@ -1,56 +1,79 @@
+from typing import Union
+
 from django.test import TestCase, Client
 from django.urls import reverse
+from model_bakery import baker
 
 from expenses.models import Account, Category, Expense
 from datetime import date
 from decimal import Decimal
 
 
-class ExpenseCreateAPITests(TestCase):
+class BaseExpenseTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         self.account = Account.objects.create(name="Test Account")
         self.category = Category.objects.create(name="Test Category")
-        self.expense_data = {
+        self.url = reverse("expenses")
+        self.expense_json_data: dict[str, Union[str, int]] = {
             "date": date.today().isoformat(),
-            "amount": str(Decimal("100.00")),
+            "amount": "100.00",
             "account": self.account.id,
             "category": self.category.id,
             "recipient": "Test Recipient",
             "description": "Test Description",
         }
-        self.url = reverse("expenses")
 
+    def create_expense(self):
+        return baker.make(
+            Expense,
+            date=self.expense_json_data["date"],
+            amount=self.expense_json_data["amount"],
+            account=self.account,
+            category=self.category,
+            recipient=self.expense_json_data["recipient"],
+            description=self.expense_json_data["description"],
+        )
+
+
+class ExpenseCreateAPITests(BaseExpenseTestCase):
     def test_create_expense(self):
         response = self.client.post(
-            self.url, self.expense_data, content_type="application/json"
+            self.url, self.expense_json_data, content_type="application/json"
         )
         self.assertEqual(response.status_code, 201)
         self.assertTrue(
-            Expense.objects.filter(recipient=self.expense_data["recipient"]).exists()
+            Expense.objects.filter(
+                recipient=self.expense_json_data["recipient"]
+            ).exists()
         )
 
     def test_create_expense_returns_correct_data(self):
         response = self.client.post(
-            self.url, self.expense_data, content_type="application/json"
+            self.url, self.expense_json_data, content_type="application/json"
         )
         self.assertEqual(response.status_code, 201)
 
         returned_expense = response.json()
-        self.assertEqual(returned_expense["date"], self.expense_data["date"])
+        self.assertEqual(returned_expense["date"], self.expense_json_data["date"])
         self.assertEqual(
-            Decimal(returned_expense["amount"]), Decimal(self.expense_data["amount"])
+            Decimal(returned_expense["amount"]),
+            Decimal(self.expense_json_data["amount"]),
         )
-        self.assertEqual(returned_expense["account"], self.expense_data["account"])
-        self.assertEqual(returned_expense["category"], self.expense_data["category"])
-        self.assertEqual(returned_expense["recipient"], self.expense_data["recipient"])
+        self.assertEqual(returned_expense["account"], self.expense_json_data["account"])
         self.assertEqual(
-            returned_expense["description"], self.expense_data["description"]
+            returned_expense["category"], self.expense_json_data["category"]
+        )
+        self.assertEqual(
+            returned_expense["recipient"], self.expense_json_data["recipient"]
+        )
+        self.assertEqual(
+            returned_expense["description"], self.expense_json_data["description"]
         )
 
     def test_create_expense_returns_valid_id(self):
         response = self.client.post(
-            self.url, self.expense_data, content_type="application/json"
+            self.url, self.expense_json_data, content_type="application/json"
         )
         self.assertEqual(response.status_code, 201)
 
@@ -60,21 +83,10 @@ class ExpenseCreateAPITests(TestCase):
         self.assertTrue(Expense.objects.filter(id=returned_expense["id"]).exists())
 
 
-class ExpenseListAPITests(TestCase):
+class ExpenseListAPITests(BaseExpenseTestCase):
     def setUp(self):
-        self.client = Client()
-        self.account = Account.objects.create(name="Test Account")
-        self.category = Category.objects.create(name="Test Category")
-        self.expense_data = {
-            "date": date.today().isoformat(),
-            "amount": str(Decimal("100.00")),
-            "account": self.account,
-            "category": self.category,
-            "recipient": "Test Recipient",
-            "description": "Test Description",
-        }
-        self.expenses = [Expense.objects.create(**self.expense_data) for _ in range(5)]
-        self.url = reverse("expenses")
+        super().setUp()
+        self.expenses = [self.create_expense() for _ in range(5)]
 
     def test_list_expenses(self):
         response = self.client.get(self.url, content_type="application/json")
